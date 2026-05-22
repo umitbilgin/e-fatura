57ff74a199ce8036c78075942eb69c1fea39ca18
umitbilgin <umitbilgintr@gmail.com>
2026-05-23 02:51:50 +0300

    fix(install-hooks): ensure hooks directory exists before creating symlinks

--- install-hooks.sh
+++ install-hooks.sh
@@ -6,7 +6,7 @@ install_hook() {
   # shellcheck disable=SC2155
   local hook_path="$(pwd)/$1-hook.sh"
 
-  if [ -f "$hook_path" ] && [ ! -L ".git/hooks/$1" ]; then
+  if [ -f "$hook_path" ] && [ -d ".git/hooks" ] && [ ! -L ".git/hooks/$1" ]; then
     ln -sf "$hook_path" ".git/hooks/$1"
   fi
 }
